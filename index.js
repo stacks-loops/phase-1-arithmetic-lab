f30da9aa930184d8f7c2c3a984f872a7e4e39586
Parker <p.cronin.connolly@gmail.com>
2024-01-09 11:50:38 -0500

    maffs

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-
 // Write your code here
+let num1 = 31
+let num2 = 2
+
+let multiply = num1 * num2
+
+let random = Math.floor(Math.random() *1000)
+
+let num3 = 104
+let num4 = 25
+
+const mod = num3 % num4
+
+let set = [1, 2, 5, 12, 3, 20, 4]
+
+const max = Math.max(...set)
